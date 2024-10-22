e5a36323ca5ffa43baef291c1b5600e892b9d4f6
PeterLeSouchu <peter22510@gmail.com>
2024-10-22 18:08:20 +0200

    feat: add navbar links

--- src/components/public/Header.tsx
+++ src/components/public/Header.tsx
@@ -1,4 +1,14 @@
 function Header() {
-  return <h1>Header Public</h1>;
+  return (
+    <nav className="flex justify-between p-7">
+      <a href="/">Accueil</a>
+      <div>
+        <a href="/login" className="mr-5">
+          Se connecter
+        </a>
+        <a href="/signup">S&apos;inscrire</a>
+      </div>
+    </nav>
+  );
 }
 export default Header;
