0cda3d2a66351cf7280f567ffac9de3c647bc6dc
PeterLeSouchu <peter22510@gmail.com>
2024-11-05 11:03:46 +0100

    fix: remove useless useEffect in layout component

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,24 +1,8 @@
-import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from '../components/private/Footer';
 import Header from '../components/private/Header';
-import { useUserStore } from '../store';
 
 function PrivateLayout() {
-  const navigate = useNavigate();
-  const { logged } = useUserStore();
-
-  /* 
-  Using useEffect to verify if user is connected in addition to the verification done in the private route component
- */
-  useEffect(() => {
-    function isLogged() {
-      if (!logged) {
-        navigate('/');
-      }
-    }
-    isLogged();
-  }, [logged, navigate]);
   return (
     <>
       <Header />
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from '../components/public/Footer';
 import Header from '../components/public/Header';
-import { useUserStore } from '../store';
 
 function PublicLayout() {
-  const navigate = useNavigate();
-  const { logged } = useUserStore();
-
-  useEffect(() => {
-    if (logged) {
-      navigate('/dashboard');
-    }
-  }, [logged, navigate]);
-
   return (
     <div className="bg-grid-pattern">
       <Header />
