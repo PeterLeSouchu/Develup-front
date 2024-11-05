9793b872520e12e065c9fc15595176f56af57546
PeterLeSouchu <peter22510@gmail.com>
2024-11-05 18:40:13 +0100

    feat: add new design

--- src/components/private/Header.tsx
+++ src/components/private/Header.tsx
@@ -44,74 +44,76 @@ function Header() {
   }
   return (
     <>
-      <header className="h-screen min-w-56  pt-2 rounded-r-3xl bg-gradient-to-b from-lightgold2 to-lightgold flex flex-col  dark:from-darkgold2 dark:to-darkgold ">
-        <img src={logo} alt="logo Develup" className="w-40 pl-2 " />
-        <nav className="flex flex-col flex-grow">
-          <Link
-            to="/dashboard/search"
-            className="py-5 pt-7 text-xl pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center "
-          >
-            <IoIosSearch className="mr-2" />
-            Recherche
-          </Link>
-          <Link
-            to="/dashboard/search"
-            className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
-          >
-            <GrProjects className="mr-2" />
-            Projets
-          </Link>
-          <Link
-            to="/dashboard/search"
-            className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
-          >
-            <FaRegMessage className="mr-2" />
-            Message
-          </Link>
-          <Link
-            to="/dashboard/search"
-            className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
-          >
-            <CgProfile className="mr-2" />
-            Profil
-          </Link>
-          <div className="flex-grow flex flex-col justify-end">
-            <span className="pl-3 py-3 ">
-              <button
-                className={`toggle-btn py-3 pl-4 ${darkTheme ? 'toggled' : ''}`}
-                onClick={() => setDarkTheme(!darkTheme)}
-                type="button"
-                aria-label="theme button"
+      <header className="h-screen min-w-56 flex  p-2  ">
+        <div className="rounded-2xl flex-grow bg-gradient-to-b from-lightgold2 to-lightgold flex flex-col  dark:from-darkgold2 dark:to-darkgold">
+          <img src={logo} alt="logo Develup" className="w-40 pl-2 pt-2 " />
+          <nav className="flex flex-col flex-grow">
+            <Link
+              to="/dashboard/search"
+              className="py-5 pt-7 text-xl pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center "
+            >
+              <IoIosSearch className="mr-2" />
+              Recherche
+            </Link>
+            <Link
+              to="/dashboard/search"
+              className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
+            >
+              <GrProjects className="mr-2" />
+              Projets
+            </Link>
+            <Link
+              to="/dashboard/search"
+              className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
+            >
+              <FaRegMessage className="mr-2" />
+              Message
+            </Link>
+            <Link
+              to="/dashboard/search"
+              className="text-xl py-5 pl-4 hover:bg-black transition hover:bg-opacity-20 flex items-center"
+            >
+              <CgProfile className="mr-2" />
+              Profil
+            </Link>
+            <div className="flex-grow flex flex-col justify-end">
+              <span className="pl-3 py-3 ">
+                <button
+                  className={`toggle-btn py-3 pl-4 ${darkTheme ? 'toggled' : ''}`}
+                  onClick={() => setDarkTheme(!darkTheme)}
+                  type="button"
+                  aria-label="theme button"
+                >
+                  <div className="thumb">
+                    <FaMoon />
+                  </div>
+                </button>
+              </span>
+              <Link
+                to="/dashboard/general-conditions-of-use"
+                className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
               >
-                <div className="thumb">
-                  <FaMoon />
-                </div>
+                <FaShieldAlt className="mr-2" />
+                CGU
+              </Link>
+              <Link
+                to="/dashboard/legal-notices"
+                className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
+              >
+                <FaRegFile className="mr-2" />
+                Mentions légales
+              </Link>
+              <button
+                type="button"
+                onClick={handleLogout}
+                className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
+              >
+                <BiLogOut className="mr-2 font-bold" />
+                Se déconnecter
               </button>
-            </span>
-            <Link
-              to="/dashboard/general-conditions-of-use"
-              className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
-            >
-              <FaShieldAlt className="mr-2" />
-              CGU
-            </Link>
-            <Link
-              to="/dashboard/legal-notices"
-              className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
-            >
-              <FaRegFile className="mr-2" />
-              Mentions légales
-            </Link>
-            <button
-              type="button"
-              onClick={handleLogout}
-              className="text-sm py-3 pl-4 w-full hover:bg-black transition hover:bg-opacity-20 flex items-center"
-            >
-              <BiLogOut className="mr-2 font-bold" />
-              Se déconnecter
-            </button>
-          </div>
-        </nav>
+            </div>
+          </nav>
+        </div>
       </header>
       <BackErrorNotification />
     </>
