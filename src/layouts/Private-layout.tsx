0cbd62d391871e1575648488d7e1dd07c94228a8
PeterLeSouchu <peter22510@gmail.com>
2024-11-08 00:16:34 +0100

    feat: add suggestion (fixed)

--- src/layouts/Private-layout.tsx
+++ src/layouts/Private-layout.tsx
@@ -7,13 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function PrivateLayout() {
   const { darkTheme } = useUserStore();
-  const matches = useMediaQuery('(max-width: 768px)');
+  const matches = useMediaQuery('(max-width: 1024px)');
   return (
     <div className={`${darkTheme && 'dark'}`}>
       <div className="md:flex md:h-screen min-h-screen transition-colors duration-500 bg-white  dark:bg-darkTheme">
         {matches ? <HeaderMobile /> : <HeaderDesktop />}
 
-        <main className="dark:text-white overflow-scroll w-full md:px-12  py-6 md:pt-6 pt-24 md:pb-6  ">
+        <main className="dark:text-white overflow-scroll w-full lg:px-12  py-6 lg:pt-6 pt-24 lg:pb-6  ">
           <Outlet />
         </main>
       </div>
--- src/pages/private/Search.tsx
+++ src/pages/private/Search.tsx
@@ -1,9 +1,65 @@
+import { useState } from 'react';
 import { IoSearch } from 'react-icons/io5';
 import { RxCross2 } from 'react-icons/rx';
 import { LogoTechno } from '../../types';
 
 function Search() {
-  // Function to return 5 technos logo and +"x" if necessary
+  // simulate db
+  const techno = [
+    {
+      id: 1,
+      name: 'react',
+      url: 'https://i.postimg.cc/W1M4FJ5d/317755-badge-html-html5-achievement-award-icon.png',
+    },
+    {
+      id: 2,
+      name: 'js',
+      url: 'https://i.postimg.cc/W1M4FJ5d/317755-badge-html-html5-achievement-award-icon.png',
+    },
+    {
+      id: 2,
+      name: 'jll',
+      url: 'https://i.postimg.cc/W1M4FJ5d/317755-badge-html-html5-achievement-award-icon.png',
+    },
+    {
+      id: 2,
+      name: 'jsk',
+      url: 'https://i.postimg.cc/W1M4FJ5d/317755-badge-html-html5-achievement-award-icon.png',
+    },
+  ];
+
+  const [suggestTechno, setSuggestTechno] = useState<
+    { name: string; id: number; url: string }[]
+  >([]);
+
+  const [inputValue, setInputValue] = useState<string>('');
+
+  function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value.toLowerCase();
+    setInputValue(e.target.value);
+
+    const filteredTechno = techno.filter((tech) =>
+      tech.name.toLowerCase().includes(value)
+    );
+
+    if (value === '') {
+      setSuggestTechno([]);
+    } else {
+      setSuggestTechno(filteredTechno);
+    }
+  }
+
+  function handleSelectTechno(technoSuggestion: {
+    name: string;
+    id: number;
+    url: string;
+  }) {
+    console.log(technoSuggestion);
+    setSuggestTechno([]);
+    setInputValue('');
+  }
+
+  // Function to return 5 technos logo and +"x" if necessary for card project
   function technoLogo(array: LogoTechno[]) {
     const displayLimit = 6;
     const extraImagesCount = array.length - displayLimit;
@@ -36,7 +92,7 @@ function Search() {
   return (
     <div className="dark:text-black">
       <form
-        className="min-h-12  w-3/4 m-auto  sm:bg-white2 sm:dark:bg-slate-200 sm:border-2 rounded-3xl items-center overflow-hidden max-w-4xl min-w-80   "
+        className="min-h-12  w-3/4 m-auto  sm:bg-white2 sm:dark:bg-slate-200 sm:border-2 rounded-3xl items-center  max-w-4xl min-w-80   "
         onSubmit={(event) => console.log(event)}
       >
         <div className=" block sm:hidden mb-2 mt-4 dark:bg-slate-200 bg-white2 border-2 rounded-3xl p-1 ">
@@ -50,17 +106,50 @@ function Search() {
             <option value="rapide">Rapide</option>
           </select>
         </div>
-        <div className="flex flex-row h-full sm:m-0 mt-2  items-center bg-white2 sm:bg-transparent dark:bg-slate-200 sm:border-none border-2 rounded-3xl sm:rounded-none overflow-hidden">
-          <div className="flex-grow p-1">
+        <div className="flex flex-row h-full sm:m-0 mt-2   items-center bg-white2 sm:bg-transparent dark:bg-slate-200 sm:border-none border-2 rounded-3xl  ">
+          <div className="relative flex-grow rounded-3xl p-1">
             <input
+              onChange={(e) => handleChangeInput(e)}
               required
+              value={inputValue}
               type="text"
               placeholder="Rechercher une techno"
-              className="h-full w-full p-2 outline-none bg-transparent pl-3 rounded-l-3xl"
+              className="h-full w-full p-2  outline-none bg-transparent pl-3 rounded-l-3xl"
             />
+            {suggestTechno.length > 0 && (
+              <>
+                <div
+                  aria-label="close side bar"
+                  onKeyDown={() => setSuggestTechno([])}
+                  role="button"
+                  tabIndex={0}
+                  className="fixed inset-0 z-20   cursor-default"
+                  onClick={() => setSuggestTechno([])}
+                />
+                <div className="absolute top-full left-0 z-30  p-2  w-full mt-1 dark:bg-white2 bg-slate-300  rounded-md shadow-md  overflow-scroll ">
+                  {suggestTechno.map(
+                    (suggestion: { name: string; id: number; url: string }) => (
+                      <button
+                        onClick={() => handleSelectTechno(suggestion)}
+                        type="button"
+                        className="dark:hover:bg-slate-200 hover:bg-white2 transition w-full flex items-center justify-start gap-2 p-1"
+                        key={suggestion.id}
+                      >
+                        <img
+                          src={suggestion.url}
+                          alt={suggestion.name}
+                          className="h-5"
+                        />
+                        {suggestion.name}
+                      </button>
+                    )
+                  )}
+                </div>
+              </>
+            )}
           </div>
-          <span className="w-px h-11 bg-gray-300 hidden sm:block  dark:bg-slate-400 mx-2" />
-          <div className="flex-grow hidden sm:block">
+          <span className="w-px h-11 bg-gray-300 hidden sm:block  dark:bg-slate-400 " />
+          <div className="ml-2 flex-grow hidden sm:block">
             <select
               required
               className="h-full w-full p-2 outline-none bg-transparent rounded-r-3xl"
@@ -73,7 +162,7 @@ function Search() {
           </div>
           <button
             type="submit"
-            className="ml-2 px-2 h-12 hover:bg-slate-300 dark:hover:bg-slate-400 transition "
+            className="ml-2 px-2 h-12 hover:bg-slate-300 rounded-r-full dark:hover:text-white2 dark:hover:bg-slate-400 transition "
             aria-label="Valider la recherche"
           >
             <IoSearch />
@@ -131,200 +220,6 @@ function Search() {
         </span>
       </div>
       <section className="flex justify-center gap-6 flex-wrap mt-10">
-        <div className="bg-white2 dark:bg-slate-200 h-99 w-72 rounded-lg border-2 p-3 flex flex-col relative ">
-          <span className="text-sm absolute right-2 top-2 p-1 bg-gold rounded-xl dark:bg-darkgold">
-            5 à 8h/semaine
-          </span>
-          <img
-            className="h-40 mx-auto"
-            src="https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png"
-            alt="nextjs"
-          />
-          <h3 className="text-2xl  my-3 line-clamp-2 break-words">
-            Titresddjzkdzzdddddddzdzdzzdzdzddzzzdz
-          </h3>
-          <p className=" text-sm line-clamp-6  my-3 break-words ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
-            eius quis odit officia minus necessitatibus corporis magnam quae,
-            sunt quosdfddd dzddzdzn
-            dzddfzfdzdzdzdzzdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkzdizdjizejzdikzdjzdizddjzizdjdzizjdizdjzdijzdidzjzdijzdjzdizdjzdidzjdzij
-          </p>
-          {technoLogo([
-            {
-              name: 'react',
-              id: 1,
-              url: 'https://i.postimg.cc/JhydY1ZW/7423888-react-react-native-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 2,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 3,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 4,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 5,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 6,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 7,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-          ])}
-        </div>
-        <div className="bg-white2 dark:bg-slate-200 h-99 w-72 rounded-lg border-2 p-3 flex flex-col relative ">
-          <span className="text-sm absolute right-2 top-2 p-1 bg-gold rounded-xl dark:bg-darkgold">
-            5 à 8h/semaine
-          </span>
-          <img
-            className="h-40 mx-auto"
-            src="https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png"
-            alt="nextjs"
-          />
-          <h3 className="text-2xl  my-3 line-clamp-2 break-words">
-            Titresddjzkdzzdddddddzdzdzzdzdzddzzzdz
-          </h3>
-          <p className=" text-sm line-clamp-6  my-3 break-words ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
-            eius quis odit officia minus necessitatibus corporis magnam quae,
-            sunt quosdfddd dzddzdzn
-            dzddfzfdzdzdzdzzdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkzdizdjizejzdikzdjzdizddjzizdjdzizjdizdjzdijzdidzjzdijzdjzdizdjzdidzjdzij
-          </p>
-          {technoLogo([
-            {
-              name: 'react',
-              id: 1,
-              url: 'https://i.postimg.cc/JhydY1ZW/7423888-react-react-native-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 2,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 3,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 4,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 5,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 6,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 7,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-            {
-              name: 'Javacript',
-              id: 8,
-              url: 'https://i.postimg.cc/G3g0Kh4q/4373213-js-logo-logos-icon.png',
-            },
-          ])}
-        </div>
         <div className="bg-white2 dark:bg-slate-200 h-99 w-72 rounded-lg border-2 p-3 flex flex-col relative ">
           <span className="text-sm absolute right-2 top-2 p-1 bg-gold rounded-xl dark:bg-darkgold">
             5 à 8h/semaine
